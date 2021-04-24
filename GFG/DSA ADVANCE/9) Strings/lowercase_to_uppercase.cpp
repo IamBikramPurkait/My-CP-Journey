f7cea8594b6bc8cf612c4876c7311af66842231c
Bikram <bkrmprkt@gmail.com>
2021-04-24 13:00:57 +0530

    add  code in string in dsa advance

---
+++ GFG/DSA ADVANCE/9) Strings/lowercase_to_uppercase.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    string caseConversion(string str)
+    {
+
+        //Your code here
+        transform(str.begin(), str.end(), str.begin(), ::toupper);
+        return str;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.caseConversion(s) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
---
+++ GFG/DSA ADVANCE/9) Strings/pattern_serach.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+// C++ program for Naive Pattern
+// Searching algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    //Function to check if the given pattern exists in the given string or not.
+    bool search(string pat, string txt)
+    {
+        // Your code here
+        size_t found = txt.find(pat);
+        if (found != string::npos)
+            return true;
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s, p;
+        cin >> s >> p;
+        Solution obj;
+        if (obj.search(p, s))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+    }
+
+    return 0;
+} // } Driver Code Endsই
