c652962cf2a5450f56d79b6f00699edd431deb17
Bikram <bkrmprkt@gmail.com>
2021-04-25 17:12:54 +0530

    update raedme

---
+++ GFG/C++ BASIC/1 - Introduction, Variables and Operators/print_gfg.cpp
@@ -0,0 +1,29 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+void print(){
+    
+    // code here
+    cout<<"\"GeeksForGeeks\"";
+}
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        print();
+        cout<<endl;
+    }
+    
+}  // } Driver Code Ends
